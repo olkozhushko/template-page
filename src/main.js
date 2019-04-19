b67ba4b183299017bd8832e00907cc311687683d
Oleh Kozhushko <olkozhushko@gmail.com>
2019-04-19 23:32:53 +0300

    some changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import "./scss/main.scss";
 import "./scss/header.scss";
 import "./img/bg_header.jpg";
+import "./img/logo.png";
+import "./img/ipad.png";
 import "./scss/nav.scss";
 import "./scss/servicesSection.scss";
+import "./scss/featuresSection.scss";
 
 import "../node_modules/@fortawesome/fontawesome-free/js/all";
 
