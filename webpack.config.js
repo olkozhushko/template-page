70b914e542a352f3812cbd00798f5210b2244f95
Oleh Kozhushko <olkozhushko@gmail.com>
2019-05-23 19:31:45 +0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
                 browsers: ["last 2 versions"]
               },
               sourceMap: isDevelopment,
-              plugins: () => [autoprefixer]
+              // plugins: () => [autoprefixer]
             }
           },
           {
