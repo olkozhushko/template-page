dd8b4a4664792e4cd4edfb15d260f36c7331a7fb
Oleh <olkozhushko@gmail.com>
2019-05-24 15:27:46 +0300

    minor changes

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const path = require("path");
+const favicon = require('express-favicon');
 
 
 const app = express();
 
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
 const port = process.env.PORT || 5000;
 
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(port, () => console.log("port is listening to"));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port);
